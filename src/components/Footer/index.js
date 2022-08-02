522489c82918ff12e6481b3cfa8789cb835763a5
Priyam Carto <cpriyam90@gmail.com>
2022-08-01 23:59:51 -0400

    footer created

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+//https://www.youtube.com/watch?v=58RRl1JTJvY - video how to use font awesome
+// font awesome for icons
+
+import React from "react";
+
+
+const Footer = () => (
+  <div className="footer">
+    <a href="https://github.com/cpriyam90" className="github">
+    <i className="fa-brands fa-github"></i>
+    </a>
+    <a href="https://www.linkedin.com/" className="linkedin">
+    <i className="fa-brands fa-linkedin"></i>
+    </a>
+  </div>
+);
+
+export default Footer;
