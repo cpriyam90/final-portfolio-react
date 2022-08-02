79bc6317cd6557bb662db93cc295c297e9cc4e7d
Priyam Carto <cpriyam90@gmail.com>
2022-08-01 23:54:04 -0400

    about component created

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+
+  return (
+    <div className="container col-12 h-50 text-center" id="#about">
+      <h5 className="lead">Priyam Chirawawala</h5>
+      <p>I am a recent graduate of the jsjdjhdhncjcf</p>
+</div>
+  )
+}
+
+export default About
