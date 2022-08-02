2e7f04f1fc86ce342e3823f39c976a720c258358
Priyam Carto <cpriyam90@gmail.com>
2022-08-02 00:03:50 -0400

    projects added

---
+++ src/components/Projects/index.js
@@ -0,0 +1,41 @@
+
+const Projects = 
+
+[{
+    name: 'Run-Buddy',
+    category: 'portfolio',
+    description: 'Website for fitness page using HTML and CSS',
+    filepath: './assets/portfolio/0.jpg',
+},
+{
+    name: 'Work-Day Planner',
+    category: 'portfolio',
+    description: 'Planner that uses HTML,CSS,JQuery, and Localstorage',
+    filepath: './assets/portfolio/1.jpg',
+},
+{
+    name: 'Travel-Easy',
+    category: 'portfolio',
+    description: 'Travel app made using HTML, CSS, JS and API calls from Hotels.com and Weather.com',
+    filepath: './assets/portfolio/2.jpg',
+},
+{
+    name: 'Budget-Tracker',
+    category: 'portfolio',
+    description: 'Budget app that uses Express, PWA and IndexedDb for offline functionality',
+    filepath: './assets/portfolio/3.jpg',
+},
+{
+    name: 'Quiz-Project',
+    category: 'js',
+    description: 'Multiple choice quiz using HTML, CSS, and JS',
+    filepath: './assets/portfolio/4.jpg',
+},
+{
+    name: 'Weather-DashBoard',
+    category: 'js',
+    description: 'Application that uses HTML, CSS, JS and API calls from weather api',
+    filepath: './assets/portfolio/5.jpg',
+}]
+
+export default Projects;
