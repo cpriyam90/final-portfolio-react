4f1151e6f5dd88eabf853dbf2ebbc0029349e20a
Priyam Carto <cpriyam90@gmail.com>
2022-08-01 23:34:28 -0400

    app.js created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Container from './components/Container';
+import ContactForm from './components/Contact';
+import Footer from './components/Footer';
+import './portfolio.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <About/>
+      <Container/>
+      <ContactForm/>
+      <Footer/>
     </div>
   );
 }
