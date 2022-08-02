43e54f7292bf2bc40dd85d951766c4dec7ed5a0b
Priyam Carto <cpriyam90@gmail.com>
2022-08-02 00:44:41 -0400

    images renamed

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,32 +4,32 @@ const Projects =
 [{
     name: 'Run-Buddy',
     description: 'Website for fitness page using HTML and CSS',
-    filepath: './assets/portfolio/0.jpg',
+    filepath: './assets/run.jpg',
 },
 {
     name: 'Work-Day Planner',
     description: 'Planner that uses HTML,CSS,JQuery, and Localstorage',
-    filepath: './assets/portfolio/1.jpg',
+    filepath: './assets/plan.jpg',
 },
 {
     name: 'Travel-Easy',
     description: 'Travel app made using HTML, CSS, JS and API calls from Hotels.com and Weather.com',
-    filepath: './assets/portfolio/2.jpg',
+    filepath: './assets/vacation.jpg',
 },
 {
     name: 'Budget-Tracker',
     description: 'Budget app that uses Express, PWA and IndexedDb for offline functionality',
-    filepath: './assets/portfolio/3.jpg',
+    filepath: './assets/budget.jpg',
 },
 {
     name: 'Quiz-Project',
     description: 'Multiple choice quiz using HTML, CSS, and JS',
-    filepath: './assets/portfolio/4.jpg',
+    filepath: './assets/quiz.jpg',
 },
 {
     name: 'Weather-DashBoard',
     description: 'Application that uses HTML, CSS, JS and API calls from weather api',
-    filepath: './assets/portfolio/5.jpg',
+    filepath: './assets/weather.jpg',
 }]
 
 export default Projects;
